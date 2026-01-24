f81e7f673dc39878d20f410e9d4d67f10b29acfc
ilxp <61609258+ilxp@users.noreply.github.com>
2026-01-24 10:30:24 +0800

    Update lede-diy.sh

--- openwrt/scripts/lede-diy.sh
+++ openwrt/scripts/lede-diy.sh
@@ -125,9 +125,9 @@ cp -rf ./diydata/data/bbr3-yaof6.12/* ./target/linux/generic/backport-6.12/
 sed -i 's/kmod-tcp-bbr/kmod-tcp-bbr3/g' feeds/luci/applications/luci-app-turboacc/Makefile
 
 # Modules  （package/kernel/linux/modules）  
-rm -rf package/kernel/linux/modules/hwmon.mk #修改CONFIG_ALL_KMODS
+#rm -rf package/kernel/linux/modules/hwmon.mk #修改CONFIG_ALL_KMODS
 rm -rf package/kernel/linux/modules/netsupport.mk   #tcp-bbr为tcp-bbr3
-cp -rf ./diydata/data/modules-lede/hwmon.mk ./package/kernel/linux/modules/
+#cp -rf ./diydata/data/modules-lede/hwmon.mk ./package/kernel/linux/modules/
 cp -rf ./diydata/data/modules-lede/netsupport.mk ./package/kernel/linux/modules/
 
 # kenrel Vermagic （安装ipk需要内核验证）即sbwml的 01-prepare_base-mainline.sh中的代码
@@ -141,7 +141,7 @@ grep HASH include/kernel-$kernel_version | awk -F'HASH-' '{print $2}' | awk '{pr
 sed -i 's/Os/O3 -mtune=generic/g' include/target.mk  #sbwml的target-modify_for_x86_64.patch代码
 
 # All-komd -Fix x86 - CONFIG_ALL_KMODS（lede无法使用导致编译b53失败。已经失效，已经改在data/hwmon.mk）
-#sed -i 's/hwmon, +PACKAGE_kmod-thermal:kmod-thermal/hwmon/g' package/kernel/linux/modules/hwmon.mk
+sed -i 's/hwmon, +PACKAGE_kmod-thermal:kmod-thermal/hwmon/g' package/kernel/linux/modules/hwmon.mk
 
 # 固件版本号(21.3.2 %y : 年份的最后两位数字)
 #date=`TZ=UTC-8 date +%m.%d.%Y`  #升级用，统一这样
